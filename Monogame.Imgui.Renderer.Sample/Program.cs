7a6beac37293138668af851995d2c452d6a52b86
Adam Beardow <abeardow13@gmail.com>
2021-11-22 21:01:01 +0000

    Initial commit

---
+++ Monogame.Imgui.Renderer.Sample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Monogame.Imgui.Renderer.Sample
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
