fafe9d06f0715209706ddcd0e5c2cb71b0fb67f5
Adam Beardow <abeardow13@gmail.com>
2021-12-20 20:30:03 +0000

    Changed the support .NET version to .NET standard 2

---
+++ Monogame.Imgui.Renderer.Sample.UWP/Game1.cs
@@ -0,0 +1,58 @@
+﻿using ImGuiNET;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Monogame.Imgui.Renderer.Sample.UWP
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private ImGuiRenderer _imGuiRenderer;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            _imGuiRenderer = new ImGuiRenderer(this);
+            _imGuiRenderer.RebuildFontAtlas();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _imGuiRenderer.BeforeLayout(gameTime);
+            ImGui.ShowDemoWindow();
+            _imGuiRenderer.AfterLayout();
+
+            base.Draw(gameTime);
+        }
+    }
+}
